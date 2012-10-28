a21c5d300f62518033db32a83cc5fa56008c1b7d
刘 理想 <liulixiang@liudeMacBook-Air.local>
2012-10-28 23:18:52 +0800

    数据抽取模块
    帖子信息抽取模块，最初版本，需要进一步修改

---
+++ webcrawler.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from BeautifulSoup import BeautifulSoup as bs
+import urllib
+
+
+def get_page(url):
+    return urllib.urlopen(url).read()
+
+
+def get_content(content):
+    head = content.find(name='h4').contents
+    reply_datetime = head[0].strip()
+    reply_username = head[1].getString()
+    reply_userid = head[1].get('href', '')
+    reply_content = content.find(name='p')
+    return reply_userid, reply_username, reply_datetime, reply_content
+
+
+def get_all_contents(page):
+    '''
+    dict_contents = {id: [name. {datetime:content, ...}], ...}
+    '''
+    soup = bs(page)
+    all_contents = soup.findAll(name='div', attrs={'class': 'reply-doc content'})
+    dict_contents = {}
+    for content in all_contents:
+        id, name, time, content = get_content(content)
+        if id in dict_contents:
+            dict_contents[id][1][time] = content
+        else:
+            dict_contents[id] = [name, {time:content}]
+    return dict_contents
+
+
+def store_contents(contents):
+    try:
+        import codecs
+        f = codecs.open('result.html', 'w', 'utf-8')
+        f.write('<!DOCTYPE html>')
+        f.write('<meta charset="utf-8">')
+        for id in contents:
+            f.write('<h3><a href=%s>%s</a><h3>' % (id, contents[id][0]))
+            for r in contents[id][1]:
+                f.write('<p class="dt">%s</p>%s' % (r, contents[id][1][r]))
+        f.close()
+        print u'成功保存文件'
+    except:
+        print u'保存文件失败'
+
+
+
+def main():
+    all = get_all_contents(get_page('http://www.douban.com/group/topic/9267121/?start=81300'))
+    store_contents(all)
+
+
+if __name__ == '__main__':
+    main()
